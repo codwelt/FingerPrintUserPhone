e7ca27d38c4c212f5a46971c38865d0f5e3b82d1
FuriosoJack <iam@furiosojack.com>
2018-12-18 08:33:31 -0500

    Se añade metodo para recoger informcion no sensible

--- fingerprintuserphone/src/main/java/com/codwelt/fingerprintuserphone/FingerprintUserPhone.java
+++ fingerprintuserphone/src/main/java/com/codwelt/fingerprintuserphone/FingerprintUserPhone.java
@@ -9,6 +9,80 @@ public class FingerprintUserPhone {
 
 
     /**
+     * Devuelve informacion de telefono no sensible
+     * @param context
+     * @param AppVersion
+     * @return
+     * @throws JSONException
+     */
+    public static JSONObject noSensitiveInformation(Context context,int AppVersion) throws JSONException
+    {
+
+        Phone phone = Phone.getInstance(context);
+        JSONObject jsObRoot = new JSONObject();
+
+
+        //Phone
+        JSONObject jsPhone = new JSONObject();
+
+        JSONObject jsSIM = new JSONObject();
+        jsSIM.put("countyISO", phone.getSIMCountryISO());
+
+        JSONObject jsSimOperator  = new JSONObject();
+
+        jsSimOperator.put("code", phone.getSIMOperatorCode());
+
+        jsSimOperator.put("name",phone.getSIMOperatorName());
+
+        jsSIM.put("operator", jsSimOperator);
+
+        jsPhone.put("sim", jsSIM);
+
+        JSONObject jsOS = new JSONObject();
+
+        jsOS.put("versionSDK",phone.getVersionSDK());
+
+        jsOS.put("name", "ANDROID");
+
+        jsPhone.put("os",jsOS);
+
+        jsPhone.put("model", phone.getModel());
+
+        jsPhone.put("manufacturer", phone.getManufacturerName());
+
+        jsObRoot.put("phone",jsPhone);
+
+
+        //Network
+        NetWork net = NetWork.getInstance(context);
+
+        JSONObject jsNet = new JSONObject();
+
+        jsNet.put("type",net.getNameType());
+
+        jsNet.put("ssid",net.getSSIDWifi());
+
+        jsObRoot.put("network",jsNet);
+
+
+
+        //App
+
+        App app = App.getInstance(context,AppVersion);
+
+        JSONObject jsApp = new JSONObject();
+
+        jsApp.put("versionCode", AppVersion);
+
+        jsApp.put("uuid",app.getUUID());
+
+        jsObRoot.put("app",jsApp);
+
+
+        return  jsObRoot;
+    }
+
+    /**
      *  Hace la construccion del json de los datos obtenidos del equipo
      * @return
      */
